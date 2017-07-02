bc5fbaf909eaf050f2e1c70a3c11ab0aba43c392
384401056 <384401056@qq.com>
2017-07-02 23:31:39 +0800

    FirstDemo/10_file.py

--- FirstDemo/08_字典.py
+++ FirstDemo/08_字典.py
@@ -1,1 +1,20 @@
-band
+#创建字典的几种方法
+
+dict1 = dict(zip("xyz","123"))
+
+dict1 = dict(name="gaoyanbin",sex="男",age=20)
+
+dict1 = {'first':1,'second':2}
+
+dict1 = dict([('a',1),('b',2),('c',3),('d',4)])
+
+print(dict1)
+
+t1 = dict1.items()
+
+t1 = dict1.keys();
+
+t1 = dict1.values();
+
+
+print(t1)
---
+++ FirstDemo/10_file.py
@@ -0,0 +1,50 @@
+#打开文件
+# myFile = open("test.txt",'r',-1,'UTF-8')
+#读取文件的所有内容
+# print(myFile.read())
+#获取当前文件指针位置
+# print(myFile.tell())
+
+#指定文件指针
+# myFile.seek(10,0)
+
+# print(myFile.readline())
+
+#将文件内容转为列表
+# list1 = list(myFile)
+#
+# print(list1)
+
+#写入数据到文件
+# myFile = open('test02.txt','w',-1,'UTF-8')
+# myFile.write('这是一个写入数据的文件!!!')
+# myFile.close()
+#
+# myFile = open('test02.txt','r',-1,'UTF-8')
+# print(myFile.read())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
