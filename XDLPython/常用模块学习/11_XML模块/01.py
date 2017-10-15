36144db6a3011e6b6c17826e17871a7f7751d077
384401056 <384401056@qq.com>
2017-10-16 00:35:07 +0800

    XML

--- XDLPython/常用模块学习/11_XML模块/01.py
+++ XDLPython/常用模块学习/11_XML模块/01.py
@@ -38,12 +38,12 @@ def f1():
         low = node.find('low').text
         print(date, high, low)
 
+
 def f2():
     '''解析本地XML'''
-    with open('test.xml','r+', encoding='utf-8') as f:
+    with open('test.xml', 'r+', encoding='utf-8') as f:
         # print(f.read())
         root = ElementTree.XML(f.read())
-
         for node1 in root:
             print(node1)
             for node2 in node1:
@@ -52,9 +52,78 @@ def f2():
                     print('       %s' % node3)
 
 
+def f3():
+    '''直接对文件进行解析, 并对XML文件进行修改.'''
+    tree = ElementTree.parse('test.xml')
+    root = tree.getroot()
+    # print(root.tag)
+    for node in root.iter('province'):
+
+        # 获取节点属性
+        print(node.attrib['id'], node.attrib['name'])
+
+        # 修改节点属性
+        node.attrib['name'] = '你的名字被修改'
+
+        # 删除节点属性
+        del node.attrib['id']
+
+        # 新增节点属性
+        node.set('ID', '000')
+        node.set('Age', '999')
+
+        # 获取节点属性的值
+        print(node.get('ID'))
+        print(node.get('name'))
+        print(node.get('Age'))
+
+    # 将所有对文件的修改写入文件
+        tree.write('test1.xml', encoding='utf-8')
+
+
+
+def f4():
+    ''' 创建XML文档 '''
+
+    # 创建根节点
+    root = ElementTree.Element('userlist')
+
+    # 创建子节点
+    user1 = ElementTree.SubElement(root, 'user', {'enable':'true'})
+
+    # 创建孙节点
+    name = ElementTree.SubElement(user1, 'name')
+    name.text = 'Jim'
+    age = ElementTree.SubElement(user1, 'age')
+    age.text = '30'
+    sex = ElementTree.SubElement(user1, 'sex')
+    sex.text = '男'
+
+    # 用另一种方法，创建子节点
+    user2 = root.makeelement('user', {'enable': 'false'})
+    # 创建孙节点
+    name = ElementTree.SubElement(user2, 'name')
+    name.text = 'Lily'
+    age = ElementTree.SubElement(user2, 'age')
+    age.text = '30'
+    sex = ElementTree.SubElement(user2, 'sex')
+    sex.text = '女'
+    root.append(user2) # 将节点添加到root节点中。
+
+
+    # 创建ElementTree,用于写入xml文件
+    tree = ElementTree.ElementTree(root)
+    # short_empty_elements=False 代表空元素不进行自闭合。
+    tree.write('new_xml.xml', short_empty_elements=False, encoding='utf-8', xml_declaration=True)
+
+    # file_xml = ElementTree.parse('new_xml.xml')
+    # root = file_xml.getroot()
+    # print(root.tag)
+
 
 def main():
-    f2()
+    f4()
+
 
 if __name__ == '__main__':
     main()
