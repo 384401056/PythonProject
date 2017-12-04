77fa85dbb92386a23514c1d1b1a1d2d8141e0c34
gaoyanbin <gaoyanbin@agrithings.cn>
2017-12-04 17:34:23 +0800

    index_bak.html

--- XDLPython/Django/test01/booktest/views.py
+++ XDLPython/Django/test01/booktest/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import *
 from django.template import RequestContext, loader
+
+
 # Create your views here.
 
 def index(request):
-    # temp = loader.get_template('booktest/index.html')
+    # temp = loader.get_template('booktest/index_bak.html')
     # return HttpResponse(temp.render())
 
     # 以上两行合为一行代码
-    return  render(request, template_name='booktest/index.html')
-
+    return render(request, template_name='booktest/index_bak.html')
