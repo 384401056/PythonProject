103c58fd712b8fce55fe3d7d0e76bc918a9aebf1
gaoyanbin <gaoyanbin@agrithings.cn>
2017-12-18 18:07:57 +0800

    network.py

--- CrazyMonitorClient/plugins/Linux/network.py
+++ CrazyMonitorClient/plugins/Linux/network.py
@@ -4,33 +4,61 @@
 import subprocess
 import locale
 
+
 def monitor(first_invoke=1):
+    """
+    IFACE：LAN接口
+    rxpck/s：每秒钟接收的数据包
+    txpck/s：每秒钟发送的数据包
+    rxbyt/s：每秒钟接收的字节数
+    txbyt/s：每秒钟发送的字节数
+    rxcmp/s：每秒钟接收的压缩数据包
+    txcmp/s：每秒钟发送的压缩数据包
+    rxmcst/s：每秒钟接收的多播数据包
+
+    平均时间:     IFACE   rxpck/s   txpck/s    rxkB/s    txkB/s   rxcmp/s   txcmp/s  rxmcst/s   %ifutil
+    平均时间:        lo     24.58     24.58      4.29      4.29      0.00      0.00      0.00      0.00
+    平均时间:      eth0    209.63    207.31    184.65     27.28      0.00      0.00      0.00      0.00
+
+    :param first_invoke:
+    :return:
+    """
     value_dic = {'status': -1}
     """执行 sar 命令，获取linux系统的返回结果，放入字典并返回"""
-    shell_command = 'sar 1 3|grep "^平均时间"'  # Linux系统命令
+    shell_command = 'sar -r 1 3|grep "^平均时间"'  # Linux系统命令
     shell_command = shell_command.encode(locale.getdefaultlocale()[1])  # 本地化中文字符
     cmd_ret = subprocess.Popen(shell_command, shell=True, stdout=subprocess.PIPE).stdout.read()
     result = str(cmd_ret, encoding='utf-8')
-    # 如果没出错
-    if result != '':
-        user, nice, system, iowait, steal, idle = result.split()[2:]  # 从字符串的第二段开始截取
-        value_dic = {
-            'user': user,
-            'nice': nice,
-            'system': system,
-            'iowait': iowait,
-            'steal': steal,
-            'idle': idle,
-            'status': 0,
-        }
 
-        # 真实的网卡数据格式。
+    if result != '':
+        result = result.split('平均时间:')[1:]
+
+        for i in len(result):
+            pass
+
         # value_dic = {
-        #     'status': 0,
         #     'data':{
-        #         'lo':{'t_in':0,'t_out':0,},
-        #         'eth0':{'t_in': 0,'t_out': 0,},
-        #     }
+        #         'lo':{
+        #             'rxpck/s':'',
+        #             'txpck/s':'',
+        #             'rxkB/s':'',
+        #             'txkB/s':'',
+        #             'rxcmp/s':'',
+        #             'txcmp/s':'',
+        #             'rxmcst/s':'',
+        #             '%ifutil':'',
+        #         },
+        #         'eth0':{
+        #             'rxpck/s': '',
+        #             'txpck/s': '',
+        #             'rxkB/s': '',
+        #             'txkB/s': '',
+        #             'rxcmp/s': '',
+        #             'txcmp/s': '',
+        #             'rxmcst/s': '',
+        #             '%ifutil': '',
+        #         },
+        #     },
         # }
 
     return value_dic
